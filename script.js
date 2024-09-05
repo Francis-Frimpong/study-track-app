aea09e1a4d1f7fbbe5118551ab9aebb898c1ee3d
bra kay <fransfrimpong26@gmail.com>
2024-09-04 17:46:51 -0700

    update code

--- script.js
+++ script.js
@@ -5,20 +5,37 @@ const seconds = document.querySelector('.seconds');
 
 let hr = 0
 let min = 0
-let sec = 0
+let sec = 1
+
+
+
 
 
 function timer() {
-    setInterval(() => {
-    
+    setInterval(() => {   
         seconds.textContent = sec++;
-        
+
         if (sec === 60) {
             sec = 0;
-            minutes.textContent = min += 1;
+            setTimeout(() => minutes.textContent = min += 1, 1000);
+        }else if (min === 60) {
+            min = 0;
+            hour.textContent = hr += 1
         }
     },1000)
+
+
+    // setInterval(() => {
+    //     minutes.textContent = min++;
+    //     if( min === 60) {
+    //         min = 0;
+    //         setTimeout(() => hour.textContent = hr += 1, 1000);
+    //     }
+    // }, 1000)
 }
+
+
+
 
  document.getElementById('start-btn').addEventListener('click', timer);
 
